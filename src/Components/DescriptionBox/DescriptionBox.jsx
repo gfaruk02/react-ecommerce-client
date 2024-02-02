27403bbe276e472fd95f327a9e5a3fe3baac82d5
gfaruk02 <gfaruk02@gmail.com>
2024-02-02 22:14:45 +0600

    create productdisplaybox component and design content

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -6,7 +6,7 @@ const DescriptionBox = () => {
             <div className="descriptionbox-navigator"> 
                 <div className="descriptionbox-nav-box"> Description
                 </div>
-                <div className="descriptionbox-nav-box fede"> Reviews (122)
+                <div className="descriptionbox-nav-box fade"> Reviews (122)
                 </div>
             </div>
             <div className="descriptionbox-description"> 
