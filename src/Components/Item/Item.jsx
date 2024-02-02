d9f05c752eb0493f2b5f7f68d9273a6be0ba7bd1
gfaruk02 <gfaruk02@gmail.com>
2024-02-03 00:40:55 +0600

    Create Related products component and desing this

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -4,7 +4,7 @@ const Item = (props) => {
     return (
         <div>
             <div className="item">
-                <Link to={`/product/${props.id}`}> <img src={props.image} alt="" /> </Link>
+                <Link to={`/product/${props.id}`}> <img onClick={window.scrollTo(0,0)} src={props.image} alt="" /> </Link>
                 <p>{props.name} </p>
             </div>
             <div className="item-prices"> 
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,11 +1,18 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import all_product from '../Components/Assets/all_product'
 
 export const ShopContext = createContext(null)
-
+const getDefaultCart = () =>{
+    let cart = {};
+    for (let index=0; index <all_product.length; index++){
+        cart[index] = 0;
+    }
+    return cart;
+}
 const ShopContextProvider = (props) =>{
 
     const contextValue = { all_product };
+    const [cartItems, setCartItems] = useState(getDefaultCart());
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
